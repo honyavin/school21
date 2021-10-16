aa1c486a3df2257f9e5b71e98933acfcfc2435b8
Honyavin Yuri <honyavin@gmail.com>
2021-10-16 17:36:18 +0300

    yjdsdj

---
+++ libft/ft_atoi.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evikings <evikings@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/16 14:08:44 by evikings          #+#    #+#             */
+/*   Updated: 2021/10/16 17:00:42 by evikings         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* 
+atoi, atoi_l -- convert ASCII string to integer
+DESCRIPTION
+     The atoi() function converts the initial portion of the string pointed to
+     by str to int representation.
+
+     It is equivalent to:
+
+           (int)strtol(str, (char **)NULL, 10);
+
+     While the atoi() function uses the current locale, the atoi_l() function
+     may be passed a locale directly. See xlocale(3) for more information.
+*/
+
+#include <libft.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_atoi(const char *str);
+{
+	int	result;
+	int	sign;
+	int	i;
+
+	result = 0
+	sign = 1
+	i = 0
+	while ((str[i] == '\n') || (str[i] == '\n') || (str[i] == '\n'))
+	{
+		/* code */
+	}
+	
+	
+	
+}	
+      
+      
